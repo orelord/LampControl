559e113636372ce1229c1d9773c99b6321be96f8
Diego Osse Fernandes <diego.fernandes@discover.com.br>
2013-07-15 13:40:22 -0300

    Initial

---
+++ LampControl/build/source/buildConfig/debug/com/uscarr/lampcontrol/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.uscarr.lampcontrol;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
---
+++ LampControl/src/main/java/com/uscarr/lampcontrol/MainActivity.java
@@ -0,0 +1,38 @@
+package com.uscarr.lampcontrol;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    private static final int REQUEST_ENABLE_BT = 0;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
